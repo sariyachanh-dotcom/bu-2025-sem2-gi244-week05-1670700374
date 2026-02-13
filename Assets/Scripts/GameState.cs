7ebac7b2b32e1c6dab8c3856dd6785a9b76f965d
sariyachanh-dotcom <sariya.chanh@bumail.net>
2026-02-13 09:49:58 +0700

    Current Enemy

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -2,8 +2,25 @@ using UnityEngine;
 
 public class GameState : MonoBehaviour
 {
+    public int hitCount = 0;
+
     void OnTriggerEnter(Collider other)
     {
+        /*if (other.gameObject.name.StartsWith("Dog"))
+        {
+           hitCount++;
+        }*/
 
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            hitCount++;
+        }
+        if (hitCount >= 10)
+        {
+            Debug.Log("GAME OVER");
+            Time.timeScale = 0;
+            //Application.Quit();
+        }
+       
     }
 }
